ff7305c2577a80dcf72519c2d447c461ebcb00eb
0xAgamy <mohamedelagamy516@gmail.com>
2026-04-26 17:15:46 +0300

    update unique file path generation

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -16,7 +16,7 @@ class DataController(BaseController):
             return False,ResponseSignal.FILE_SIZE_EXCEEDED.value
         return True, ResponseSignal.FILE_UPLOADED_SUCCESS.value
     
-    def generate_unique_filename(self,original_name:str,project_id:str):
+    def generate_unique_filepath(self,original_name:str,project_id:str):
         random_key= self.generate_random_string()
         project_path= ProjectController().get_project_path(project_id=project_id)
 
@@ -32,8 +32,8 @@ class DataController(BaseController):
                 project_path,
                 random_key + "_"+ clean_filename
             )
-            
-        return new_file_path
+
+        return new_file_path, random_key + "_"+ clean_filename
     
 
 
