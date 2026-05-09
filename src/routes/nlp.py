9d0587de1ab6717512b50f61f7f738a087fe03ca
0xAgamy <mohamedelagamy516@gmail.com>
2026-05-09 14:53:33 +0300

    fix template_parser for NLPController

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -35,7 +35,9 @@ async def index_project(request:Request, project_id:str,push_request:PushRequest
     nlp_controller=NLPController(
         vectordb_client=request.app.vectordb_client,
         generation_client=request.app.generation_client,
-        embedding_client=request.app.embedding_client
+        embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser
+
     )
     has_record=True
     page_no=1
@@ -93,7 +95,9 @@ async def get_project_index_info(request:Request, project_id:str):
     nlp_controller=NLPController(
         vectordb_client=request.app.vectordb_client,
         generation_client=request.app.generation_client,
-        embedding_client=request.app.embedding_client
+        embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser
+
     )
 
     collection_info= nlp_controller.get_vector_db_collection_info(
